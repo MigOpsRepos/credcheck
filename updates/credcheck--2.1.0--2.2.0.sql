602728570cd1cc23f45ea690811a3ed4c03f19c9
Gilles Darold <gilles@darold.net>
2024-11-18 17:50:19 +0700

    Move the project under HexaCluster GitHub account

--- updates/credcheck--2.1.0--2.2.0.sql
+++
@@ -1,6 +0,0 @@
--- credcheck extension for PostgreSQL
--- Copyright (c) 2021-2023 MigOps Inc - All rights reserved.
--- Copyright (c) 2023 Gilles Darold - All rights reserved.
-
--- No SQL change to apply in this version
-
--- updates/credcheck--2.5.0--2.6.0.sql
+++
@@ -1,5 +0,0 @@
--- credcheck extension for PostgreSQL
--- Copyright (c) 2024 HexaCluster Corp - All rights reserved.
-
--- No SQL change to apply in this version
-
